60acdcf1d41263d3f1895954c20f24e317f71db6
eldesinstruments <info@eldesinstruments.com>
2025-05-25 21:18:18 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,42 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    unoptimized: true,
+    domains: [
+      "source.unsplash.com",
+      "images.unsplash.com",
+      "ext.same-assets.com",
+      "ugc.same-assets.com",
+    ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "source.unsplash.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ext.same-assets.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ugc.same-assets.com",
+        pathname: "/**",
+      },
+    ],
+    sizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840], // richiesto da Next.js 15
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840], // richiesto anche se non usato
+    minimumCacheTTL: 60,
+    formats: ["image/webp"],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+};
 
 module.exports = nextConfig;
